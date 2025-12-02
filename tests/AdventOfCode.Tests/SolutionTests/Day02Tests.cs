567b4100b2cb35fd4ea3c43713f518afae478f29
Shane Holland <shane.m.holland@gmail.com>
2025-12-02 18:08:30 +0000

    Add solution for Day 2.

--- tests/AdventOfCode.Tests/SolutionTests/Day02Tests.cs
+++ tests/AdventOfCode.Tests/SolutionTests/Day02Tests.cs
@@ -3,22 +3,15 @@ using Xunit;
 
 namespace AdventOfCode.Tests.SolutionTests;
 
-public class Day01Tests
+public class Day02Tests
 {
-    private readonly Day01 _solution = new();
-    private static readonly string PUZZLE_INPUT = @"L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82";
+    private readonly Day02 _solution = new();
+    private static readonly string PUZZLE_INPUT = @"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124";
 
-    private static readonly string LEVEL_1_SOLUTION = "3";
-    private static readonly string LEVEL_2_SOLUTION = "6";
+    private static readonly string LEVEL_1_SOLUTION = "1227775554";
+    private static readonly string LEVEL_2_SOLUTION = "4174379265";
     
     [Fact]
     public void TestLevel1_WithSampleInput()
@@ -32,11 +25,5 @@ L82";
     {
         var result = _solution.SolveLevel2(PUZZLE_INPUT);
         Assert.Equal(LEVEL_2_SOLUTION, result);
-    }
-
-    [Fact]
-    public void TestLevel1_WithInvalidInput()
-    {
-        Assert.Throws<InvalidOperationException>(() => _solution.SolveLevel1("X4F"));
     }
 }
