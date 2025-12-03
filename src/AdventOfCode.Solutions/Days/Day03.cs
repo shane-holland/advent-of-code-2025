5de6aabb52fa2be9b72c021f5b12b1f3b4cf16bd
Shane Holland <shane.m.holland@gmail.com>
2025-12-03 13:59:17 +0000

    Add Solution for Day 3.

--- src/AdventOfCode.Solutions/Days/Day03.cs
+++ src/AdventOfCode.Solutions/Days/Day03.cs
@@ -1,9 +1,10 @@
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Solutions.Days;
 
 /// <summary>
-/// Solution for Day 3: XXXXXXX
+/// Solution for Day 3: Lobby
 /// </summary>
 public partial class Day03 : ISolution
 {
@@ -35,11 +36,55 @@ public partial class Day03 : ISolution
 
     private static long ComputeLevel1(string[] lines)
     {
-        return 0;
+        long totalJoltage = 0;
+        
+        foreach( var line in lines)
+        {
+            var joltage = ComputeLargestJoltage(line, 2);
+
+            totalJoltage += joltage;
+        }
+
+        return totalJoltage;
     }
 
     private static long ComputeLevel2(string[] lines)
     {
-        return 0;
+        long totalJoltage = 0;
+        
+        foreach( var line in lines)
+        {
+            var joltage = ComputeLargestJoltage(line, 12);
+
+            totalJoltage += joltage;
+        }
+
+        return totalJoltage;
+    }
+
+    private static long ComputeLargestJoltage(string batteryBank, int length)
+    {
+        string joltage = "";
+
+        int index = 0;
+
+        while(joltage.Length < length)
+        {
+            var digit = 0;
+            for(var i = index; i<batteryBank.Length - (length - joltage.Length - 1); i++)
+            {
+                if (int.Parse(batteryBank[i].ToString()) > digit)
+                {
+                    digit = int.Parse(batteryBank[i].ToString());
+                    index = i+1;
+                }
+            }
+            joltage += digit.ToString();
+        }
+
+
+        Console.WriteLine($"{batteryBank} <-- {joltage}");
+
+        return long.Parse(joltage);
     }
 }
